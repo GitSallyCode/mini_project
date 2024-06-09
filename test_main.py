38de47d57f35e8a334db14ca581519a70a219659
Sally Han <hansbsb0128@gmail.com>
2024-06-08 17:33:41 -0700

    "Add basics.. "

--- test_main.py
+++ test_main.py
@@ -1,1 +1,45 @@
-print("Hello World")
+tasks = []
+
+# def add_task():
+#     task = input("Please enter a task: ")
+#     tasks.append(task)
+#     print(f"{task} has been added to the list.")
+def view_task():
+    if tasks:
+        print("To-do list: ")
+def remove_task():
+    removed_task = int(input("Enter the number to delete: "))
+    if removed_task in range(len(tasks)):
+        tasks.pop(removed_task)
+        print(f"{removed_task} has been removed.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
