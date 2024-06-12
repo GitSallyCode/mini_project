8954497294a8b389bd68e37688e45bdd1aff2e0a
joaopedromendesp <joaopedroomp5@gmail.com>
2024-06-12 09:32:45 -0700

    Adjustments

--- To-do List 1.py
+++ To-do List 1.py
@@ -1,18 +1,60 @@
-def menu_list():
-     print("To-Do List Application\n 1. Add task\n 2. Remove task\n 3. View task\n 4. Exit")
+tasks = []
 
-def add_task(tasks):
-    task = input("Enter a new task: ")
-    tasks.append(task)
-    print(f"Task {task} was added to the list")
 
-def menu():
-    tasks = []
+def add_task():
     while True:
-        menu_list()
-        choice = input("Enter your choice: ")
-        if choice == "1":
-            add_task(tasks)
+        task_name = input("Please enter a task (q to quit): ")
+        if task_name.lower() == 'q':
             break
-menu()
+        priority = input("Enter priority (High, Medium, Low): ")
+        deadline = input("Enter deadline (YYYY-MM-DD): ")
+        task = {"name": task_name, "priority": priority, "deadline": deadline}
+        tasks.append(task)
+        task_details = ", ".join(f"{key.capitalize()}: {value}" for key, value in task.items())
+        print(f"[{task_details}] has been added to the list.")
 
+
+def remove_task():
+    view_task()
+    try:
+        removed_task_index = int(input("Enter the number to delete: ")) - 1
+        if removed_task_index in range(len(tasks)):
+            removed_task = tasks.pop(removed_task_index)
+            print(f"{removed_task} has been removed.")
+        else:
+            print("Invalid task number.")
+    except:
+        print("Invalid input. Please enter a valid number.")
+
+def view_task():
+    if len(tasks) == 0:
+        print("There are no tasks.")
+    else:
+        print("List of tasks are below: ")
+        print("-----------------------------------------------------------------------")
+        for i, task in enumerate(tasks, start=1):
+            task_details = ", ".join(f"{key.capitalize()}: {value}" for key, value in task.items())
+            print(f"{i}. {task_details}")
+            print("-----------------------------------------------------------------------")
+
+while True:
+    print("[To-Do List Application]")
+    print("1. Add Task")
+    print("2. Remove Task")
+    print("3. View Tasks")
+    print("4. Exit")
+    number = input("Please select a number: ")
+
+    if number == '1':
+        add_task()
+    elif number == '2':
+        remove_task()
+    elif number == '3':
+        view_task()
+    elif number == '4':
+        print("Goodbye")
+        break
+    else:
+        print("Invalid input. Please enter a valid number.")
+
+print("Thank you!:)")
